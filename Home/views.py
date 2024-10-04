d1c030a0cf0635208110eabb59e6b0219a65583b
adilmalik01 <malikadil5679@gmail.com>
2024-10-04 18:29:08 +0500

    create method complete in crud

--- Home/views.py
+++ Home/views.py
@@ -1,9 +1,10 @@
-from django.shortcuts import render
+from django.shortcuts import render , redirect
 from .models import Book
+from .form import ContactForm
 
 def RenderHome(request):
     books = Book.objects.all()
-    return render(request, "index.html", context={'books': books})
+    return render(request, "index.html", context={'books': books })
 
 def About(request):
     return render(request , "about.html")
@@ -13,4 +14,18 @@ def Service(request):
     return render(request , "service.html")
 
 def Contact(request):
-    return render(request , "contact.html")
+    if request.method == 'POST':
+        form = ContactForm(request.POST)
+        if form.is_valid():
+            form.save()
+            
+            
+        return  redirect('/') 
+    else:
+        form = ContactForm()
+    return render(request , "contact.html",{'form': form})
+           
+    
+    
+    
+    
